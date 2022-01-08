1edba7a85d5caf352c9a35315c0f4cdb9beea2a1
Bruno Padilha Rangel <brpadilha.dev@gmail.com>
2022-01-08 11:44:19 -0300

    cria primeiro teste de renderizacao para o search e configura o jest

--- components/search.spec.js
+++ components/search.spec.js
@@ -2,7 +2,7 @@ import Search from './search';
 import { render, screen } from '@testing-library/react';
 
 describe('Search', () => {
-  it('should render Search component', () => {
+  it('should render', () => {
     render(<Search />);
 
     expect(screen.getByTestId('search')).toBeInTheDocument();
